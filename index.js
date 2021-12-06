93d16d237ef1d9d090b956165bec9ae439d1a1a4
CaueJoseOliveiraFalcao <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2021-12-06 17:02:15 -0400

    upload cards

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function bar(){
    x = document.getElementById("yes").style.display = 'flex';
+   y = document.getElementById("nao").style.display = 'none';
+};
+
+function ex(){
+   x = document.getElementById("yes").style.display = 'none';
+   y = document.getElementById("nao").style.display = 'flex';
+   
 };
