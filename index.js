1376e38db6f94f114e4584ced6ba148c8331eb62
CaueJoseOliveiraFalcao <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2021-12-10 13:39:47 -0400

    upload modal html

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ function bar(){
    x = document.getElementById("yes").style.display = 'flex';
    y = document.getElementById("nao").style.display = 'none';
 };
-
 function ex(){
    x = document.getElementById("yes").style.display = 'none';
    y = document.getElementById("nao").style.display = 'flex';
-   
+};
+function bleach_modal(){
+   u = document.getElementById("full").style.display = "flex";
+};
+function blech_modal_f(){
+   u = document.getElementById("full").style.display = "none";
+};
+function html_modal(){
+   u = document.getElementById("html5").style.display = "flex";
+};
+function html_modal_f(){
+   u = document.getElementById("html5").style.display = "none";
 };
 
-function bleach_modal(){
-   u = document.getElementById("full").style.display = "flex"
 
-}
-function blech_modal_f(){
-   u = document.getElementById("full").style.display = "none"
-}
