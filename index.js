6470701989c66339db1e78202a3bef0896769564
CaueJoseOliveiraFalcao <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2021-12-14 10:31:44 -0400

    upload final

--- index.js
+++ index.js
@@ -9,22 +9,11 @@ function ex(){
    document.getElementById("nao").style.display = 'flex';
    document.getElementById("rodape_div").style.display = 'flex';
 };
-function bleach_modal(){
-   u = document.getElementById("full").style.display = "flex";
+function abertura(vari){
+   document.getElementById(vari).style.display = "flex";
 };
-function blech_modal_f(){
-   u = document.getElementById("full").style.display = "none";
+function fechamento(vari){
+   document.getElementById(vari).style.display = "none";
 };
-function html_modal(){
-   u = document.getElementById("html5").style.display = "flex";
-};
-function html_modal_f(){
-   u = document.getElementById("html5").style.display = "none";
-};
-function css_modal(){
-   u = document.getElementById('css3').style.display = "flex"
-}
-function css_modal_f(){
-   u = document.getElementById('css3').style.display = "none"
-}
+
 
